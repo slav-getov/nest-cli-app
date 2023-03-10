6592793885b2f6cd9ae675d125d9290a66070106
Preslav Getov <preslav.getov@yara.com>
2023-03-10 10:36:59 +0200

    final touch

--- src/client/client.entity.ts
+++ src/client/client.entity.ts
@@ -22,14 +22,14 @@ export class Client {
   //remember that if you just save entity directly to db hooks will not execute!!!
   @AfterInsert()
   logAfterInsert() {
-    console.log(`Inserted user with id ${this.id}`);
+    console.log(`Inserted client with id ${this.id}`);
   }
   @AfterUpdate()
   logAfterUpdate() {
-    console.log(`Updated user with id ${this.id}`);
+    console.log(`Updated client with id ${this.id}`);
   }
   @BeforeRemove()
   logAfterRemove() {
-    console.log(`User with id ${this.id} removed`);
+    console.log(`Client with id ${this.id} removed`);
   }
 }
